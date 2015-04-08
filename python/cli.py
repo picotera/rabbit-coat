64a51c099feb44c06beea84a10c24e508af9ae0c
daTokenizer <adam.inf@gmail.com>
2015-04-08 18:36:00 +0300

    configuration file and validation

--- python/cli.py
+++ python/cli.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-import rabbitcoat 
-logging.basicConfig()
+import rabbitcoat
 
 
 ####################
@@ -10,63 +9,126 @@ logging.basicConfig()
 ####################
 
 
-def usage():
-    print 'wrapper.py [--sender/--receiver] [-s <server>] [-u <user>] [-p <password>] [-v <vhost>] [-q <in_queue>] [-Q <out_queue>] [-m <message>] [-t <timeout>] [-f <configuration file>]'
-    sys.exit()
+class rabbit_coat_meat_adapter():
+    _server    = None
+    _user      = None
+    _password  = None
+    _vhost     = None
+    _timeout   = None
+    _in_queue  = None
+    _out_queue = None
+    _message   = "Ping!"
+    _callback  = rabbitcoat.default_callback
+    _conf      = rabbitcoat.default_configuration_file
+
+    default_sender = True
+
+
+    def readConfigurationFile(self,opt):
+        
+
+        for opt, arg in opts:
+            if opt in ("-S", "--sender"):
+                message  = rabbitcoat.default_message
+            elif opt in ("-R", "--receiver"):
+                callback  = rabbitcoat.default_callback
+            elif opt in ("-s", "--server"):
+                server = arg
+            elif opt in ("-u", "--user"):
+                user = arg
+            elif opt in ("-p", "--password"):
+                password = arg
+            elif opt in ("-v", "--vhost"):
+                vhost = arg
+            elif opt in ("-q", "--in_queue"):
+                in_queue = arg
+            elif opt in ("-Q", "--out_queue"):
+                out_queue = arg
+            elif opt in ("-m", "--message"):
+                message = arg
+            elif opt in ("-t", "--timeout"):
+                timeout = arg
+            elif opt in ("-f", "--conf"):
+                confFile = arg
+            else:
+                usage()
+        #self.connection.close()
 
 
 
+    def usage(self):
+        print 'wrapper.py [--help/-h] [--sender/--receiver] [-s <server>] [-u <user>] [-p <password>] [-v <vhost>] [-q <in_queue>] [-Q <out_queue>] [-m <message>] [-t <timeout>] [-f <configuration file>]'
+        sys.exit()
 	
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv,"SRhs:u:p:v:q:Q:m:t:",
-                                   ["receiver""sender","server=",
-                                    "user=","password=","vohst=",
-                                    "in_queue=","out_queue=",
-                                    "message=","timeout="])
-    except getopt.GetoptError:
-        usage()    
-
-    for opt, arg in opts:
-        if opt in ("-h","-?","--help"):
-            usage()
-        elif opt in ("-f", "--conf","--configuration","--configurationfile"):
-            readConfigurationFile(opt)
-
-
-    confFile = server = user = password = vhost = timeout = in_queue = out_queue = message = callback = None
-    for opt, arg in opts:
-        if opt in ("-S", "--sender"):
-            message  = RabbitWrapper.default_message
-        elif opt in ("-R", "--receiver"):
-            callback  = RabbitWrapper.default_callback
-        elif opt in ("-s", "--server"):
-            server = arg
-        elif opt in ("-u", "--user"):
-            user = arg
-        elif opt in ("-p", "--password"):
-            password = arg
-        elif opt in ("-v", "--vhost"):
-            vhost = arg
-        elif opt in ("-q", "--in_queue"):
-            in_queue = arg
-        elif opt in ("-Q", "--out_queue"):
-            out_queue = arg
-        elif opt in ("-m", "--message"):
-            message = arg
-        elif opt in ("-t", "--timeout"):
-            timeout = arg
-        elif opt in ("-f", "--conf"):
-            confFile = arg
-        else:
-            usage()
-
+        
+    def __init__(self,argv):
+        try:
+            opts, args = getopt.getopt(argv,"SRhs:u:p:v:q:Q:m:t:c:f:?", #check if that '?' is working
+                                       ["receiver","help","sender",
+                                        "server=","user=","password=",
+                                        "vohst=","in_queue=",
+                                        "out_queue=","conf=",
+                                        "configuration=",
+                                        "configurationFile=",
+                                        "message=","timeout="])
+        except getopt.GetoptError:
+            usage()    
     
-    server, user, password, vhost = 
-    rabbit = RabbitWrapper(server,user,password,vhost,timeout,in_queue,out_queue,message,callback)
-    rabbit.run();
-        
-        
+            #	    server, user, password, vhost, timeout, in_queue, out_queue, message, callback = None
+        isSender = self.default_sender
+        for opt, arg in opts:
+            if opt in ("-h","-?","--help"):
+                usage()
+            elif opt in ("-S", "--sender"):
+                _isSender = True
+            elif opt in ("-R", "--receiver"):
+                _isSender = False
+            elif opt in ("-s", "--server"):
+                _server = arg
+            elif opt in ("-u", "--user"):
+                _user = arg
+            elif opt in ("-p", "--password"):
+                _password = arg
+            elif opt in ("-v", "--vhost"):
+                _vhost = arg
+            elif opt in ("-q", "--in_queue"):
+                _in_queue = arg
+            elif opt in ("-Q", "--out_queue"):
+                _out_queue = arg
+            elif opt in ("-m", "--message"):
+                _message = arg
+            elif opt in ("-t", "--timeout"):
+                _timeout = arg
+            elif opt in ("-f","-c","--conf","--configuration","--configurationfile"):
+                _confFile = arg
+            else:
+                usage()
+	
+	    
+        rabbit = rabbitcoat(_confFile,_server,_user,_password,_vhost,_timeout,_in_queue,_out_queue,_message,_callback)
+            
+
+        if (self._callback is None):
+            print "Started in Sender mode"
+            isSender = True 
+        else:
+            if (self._message is None):
+                print "Started in Receiver mode"
+                isSender = False
+                
+
+
+        if (isSender) :
+            rabbit.Send(rabbit.out_queue, rabbit.message)
+        else :
+            rabbit.Receive(rabbit.in_queue)
+            
+
+
+	        
+	        
+
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    rabbit_coat_meat_adapter(sys.argv[1:])
+    #main(sys.argv[1:])
--- python/rabbitcoat.py
+++ python/rabbitcoat.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python
-import pika, os, uuid, logging,sys, getopt
+import pika, os, uuid, logging,sys, ConfigParser
 logging.basicConfig()
 
-# Parse CLODUAMQP_URL (fallback to localhost)
-
-##urltext = 'amqp://qddxpjau:SMJ4jbIv97tiSQg7YDIw8RLMCjyWoVXg@turtle.rmq.cloudamqp.com/qddxpjau'
-##furl = os.environ.get('CLOUDAMQP_URL', urltext)#'amqp://qddxpjau:SMJ4jbIv97tiSQg7YDIw8RLMCjyWoVXg@turtle.rmq.cloudamqp.com/qddxpjau')
-###http://activemq-domainname.rhcloud.com/demo/message/OPENSHIFT/DEMO?type=topic
 
 
-class  rabbitcoat(object):
+def validate(value,current,default):
+    if ((value is None) or (value == "")):
+        if ((current is None) or (current == "")):
+            return default
+        else:
+            return current
+    else:
+        return value
 
+
+class rabbitcoat(object):
     
     ####################
     #   Defaults
     ####################
 
-    default_server  = "turtle.rmq.cloudamqp.com"
-    default_user = "qddxpjau"
-    default_password = "SMJ4jbIv97tiSQg7YDIw8RLMCjyWoVXg"
+    default_server  = "some server" #"turtle.rmq.cloudamqp.com"
+    default_user = "some user" #"qddxpjau"
+    default_password = "some password" #"SMJ4jbIv97tiSQg7YDIw8RLMCjyWoVXg"
     default_vhost = default_user
-    default_queue = "TEST"
-    default_message ="Ding!"
     default_timeout = 5
-    default_sender = True
+
+    default_queue = "TEST"
+    default_message ="Default Test Message"
+    default_configuration_file = "rabbitcoat.conf"
+
     def default_callback(self,ch, method, properties, body):
         print "Receiever: Received message with:"
-        #if (properties.corr_id is None)):
+        #if (properties.correlation_id is None)):
         #    print "\t correlation ID: %s" % properties.corr_id
         if (not (body is None)):
             print "\t body: %r" % (body)
@@ -42,34 +48,68 @@ class  rabbitcoat(object):
     #  Initializers
     ####################
 
-    def __init__(self,server=None, user=None, password=None, vhost=None, timeout=None, in_queue=None, out_queue=None, message=None, callback=None):
-        self.isSender = self.default_sender
-        if (callback is None):
-            print "Started in Sender mode"
-            self.isSender = True 
-        else:
-            if (message is None):
-                print "Started in Receiver mode"
-                self.isSender = False
-        
-        self.set_defaults(server, user, password, vhost, timeout)
+    def __init__(self,conf=None,server=None, user=None, password=None, vhost=None, timeout=None, in_queue=None, out_queue=None, message=None, callback=None):
+        self.server = self.user = self.password = self.vhost = self.timeout = self.message = self.in_queue = self.out_queue = self.callback = self.response_function = None
+
+        self.readConfigurationFile(conf)
+        self.set_infrastructure(server, user, password, vhost, timeout)
         self.set_action_parameters(in_queue, out_queue, message, callback)
         self.init_infrastructure()
 
 
-    def set_defaults(self,server=default_server, user=default_user, password=default_password,
-                     vhost=default_vhost, timeout=default_timeout):
-        if (server is None):   server =   self.default_server
-        if (user is None):     user =     self.default_user
-        if (password is None): password = self.default_password
-        if (vhost is None):    vhost =    self.default_vhost
-        if (timeout is None):  timeout =  self.default_timeout
 
-        self.server = server
-        self.user = user
-        self.password = password
-        self.vhost = vhost
-        self.timeout = timeout
+    def readConfigurationFile(self,conf=None):
+        if (conf is None):
+            configFile = self.default_configuration_file
+        else:
+            configFile = conf
+
+        config = ConfigParser.SafeConfigParser(allow_no_value=True)
+        config.read(configFile)
+        
+
+        self.server    = validate(config.get('infrastructure', 'server'),self.server,self.default_server)
+        self.user      = validate(config.get('infrastructure', 'user')    ,self.user, self.default_user)
+        self.password  = validate(config.get('infrastructure', 'password'),self.password,self.default_password)
+        self.vhost     = validate(config.get('infrastructure', 'vhost')   ,self.vhost,self.default_vhost)
+        self.timeout   = validate(config.get('infrastructure', 'timeout') ,self.timeout,self.default_timeout)
+        
+        self.in_queue  = validate(config.get('receive parameters', 'in_queue'),self.in_queue,self.default_queue)
+        self.callback  = validate(config.get('receive parameters', 'callback'),self.callback,self.default_callback)
+
+        self.out_queue = validate(config.get('send parameters', 'out_queue')  ,self.out_queue,self.default_queue)
+        self.message   = validate(config.get('send parameters', 'message')    ,self.message,self.default_message)
+
+        self.response_function =  validate(config.get('response parameters', 'response_function'),
+                                           self.response_function, self.default_response)
+ 
+        
+
+        #print self.server    
+        #print self.user
+        #print self.password  
+        #print self.vhost     
+        #print self.timeout   
+        #print "---"                
+        #print self.in_queue  
+        #print self.callback 
+        #print "---"               
+        #print self.out_queue 
+        #print self.message
+        #print "---"
+        #print self.response_function
+
+
+
+
+
+    def set_infrastructure(self,server=None, user=None, password=None,vhost=None,timeout=None):
+        self.server   = validate(server,self.server,self.default_server)
+        self.user     = validate(user,self.user,self.default_user)
+        self.password = validate(password,self.password,self.default_password)
+        self.vhost    = validate(vhost,self.vhost,self.default_vhost)
+        self.timeout  = validate(timeout,self.timeout,self.default_timeout)
+
         
 
     def init_infrastructure(self):
@@ -82,63 +122,76 @@ class  rabbitcoat(object):
         self.channel = self.connection.channel() # start a channel
 
 
+
     def set_action_parameters(self, in_queue=None, out_queue=None, message=None, callback=None):
-        self.message = self.in_queue = self.out_queue = self.callback = None
         
         print "Action parameters:"
-        self.message = self.validate_message(message)
+        self.message = validate(message,self.message,self.default_message)
         print "[.] message=%s" % self.message
 
-        self.in_queue = self.validate_in_queue(in_queue)
+        self.in_queue = validate(in_queue,self.in_queue,self.default_queue)
         print "[.] in queue=%s" % self.in_queue
 
-        self.out_queue = self.validate_out_queue(out_queue)
+        self.out_queue = validate(out_queue,self.out_queue,self.default_queue)
         print "[.] out queue=%s" % self.out_queue
+
+        self.callback = validate(callback,self.callback,self.default_callback)
+        print "[.] callback=%s" % self.callback
 
 
     ####################
     #   Validators
     ####################
 
-    def validate_message(self,message):
-        if (message is None):
-            if (self.message is None):
-                return self.default_message
+    def validate(value,current,default):
+        if (value is None):
+            if (current is None):
+                return default
             else:
-                return self.message
+                return current
         else:
-            return message
-
-
-    def validate_in_queue(self,queue):
-        if (queue is None):
-            if (self.in_queue is None):
-                return self.default_queue
-            else:
-                return self.in_queue
-        else:
-            return queue
-
-
-    def validate_out_queue(self,queue):
-        if (queue is None):
-            if (self.out_queue is None):
-                return self.default_queue
-            else:
-                return self.out_queue
-        else:
-            return queue
-
-        
-    def validate_callback(self,callback):
-        if (callback is None):
-            if (self.callback is None):
-                return self.default_callback
-            else:
-                return self.callback
-        else:
-            return callback
-
+            return value
+#
+#
+#    def validate_message(self,message):
+#        if (message is None):
+#            if (self.message is None):
+#                return self.default_message
+#            else:
+#                return self.message
+#        else:
+#            return message
+#
+#
+#    def validate_in_queue(self,queue):
+#        if (queue is None):
+#            if (self.in_queue is None):
+#                return self.default_queue
+#            else:
+#                return self.in_queue
+#        else:
+#            return queue
+#
+#
+#    def validate_out_queue(self,queue):
+#        if (queue is None):
+#            if (self.out_queue is None):
+#                return self.default_queue
+#            else:
+#                return self.out_queue
+#        else:
+#            return queue
+#
+#        
+#    def validate_callback(self,callback):
+#        if (callback is None):
+#            if (self.callback is None):
+#                return self.default_callback
+#            else:
+#                return self.callback
+#        else:
+#            return callback
+#
 
     ####################
     #
@@ -147,20 +200,11 @@ class  rabbitcoat(object):
     ####################
 
 
-    def run(self):
-        if (self.isSender) :
-            #self.channel.queue_declare(self.out_queue,durable=True) # Declare a queue
-            self.Send(self.out_queue, self.message)
-        else :
-            #self.channel.queue_declare(self.in_queue,durable=True) # Declare a queue
-            self.Receive(self.in_queue)
-        #self.connection.close()
-
 
     def Send(self,queue=None, message=None,corr_id=str(uuid.uuid4()),reply_to_queue=None):
-        queue = self.validate_out_queue(queue)
-        message = self.validate_message(message)
-        reply_to_queue = self.validate_in_queue(queue)
+        queue          = validate(queue,self.out_queue,self.default_queue)
+        message        = validate(message,self.message,self.default_message)
+        reply_to_queue = validate(reply_to_queue,self.in_queue,self.default_queue)
         
 
         #this may be not so good performance-wize since you constantly open and close the connection
@@ -180,9 +224,9 @@ class  rabbitcoat(object):
         self.connection.close()
 
 
-    def Receive(self,queue=None, callback=None):
-        queue = self.validate_in_queue(queue)
-        callback = self.validate_callback(callback)
+    def Receive(self,queue=None, callback=None,read_repeatedly=False):
+        queue    = validate(queue,self.in_queue,self.default_queue)
+        callback = validate(callback,self.callback,self.default_callback)
         channel = self.channel
 
         #this may be not so good performance-wize since you constantly open and close the connection
@@ -193,7 +237,7 @@ class  rabbitcoat(object):
         channel.basic_qos(prefetch_count=1)
         channel.basic_consume(callback,
                               queue,
-                              no_ack=False)
+                              no_ack=read_repeatedly)
         
         
         while self.response is None:
@@ -213,9 +257,7 @@ class  rabbitcoat(object):
         return "response: got %s" % str(body)
 
     def setResponseFunction(self,function=None):
-        if (function is None):
-            function = self.default_response
-        self.response_function = function
+        self.response_function = validate(function,self.response_function,self.default_response)
 
 
 
@@ -233,8 +275,8 @@ class  rabbitcoat(object):
 
 
     def startRespnseServer(self,response_function=None):
-        setResponseFunction(response_function)
-        Respond(callback=Respond)
+        self.setResponseFunction(response_function)
+        self.Receive(callback=Respond,read_repeatedly=True)
 
 
 
@@ -243,64 +285,70 @@ class  rabbitcoat(object):
         self.Receive(reply_to_queue, callback)
 
 
-
-####################
 #
-# MeatSpace Adapter
+#####################
+##
+## MeatSpace Adapter
+##
+#####################
 #
-####################
+#
+#def usage():
+#    print 'wrapper.py [--sender/--receiver] [-s <server>] [-u <user>] [-p <password>] [-v <vhost>] [-q <in_queue>] [-Q <out_queue>] [-m <message>] [-t <timeout>]'
+#    sys.exit()
+#
+#
+#
+#	
+#def main(argv):
+#    try:
+#        opts, args = getopt.getopt(argv,"SRhs:u:p:v:q:Q:m:t:",
+#                                   ["receiver""sender","server=",
+#                                    "user=","password=","vohst=",
+#                                    "in_queue=","out_queue=",
+#                                    "message=","timeout="])
+#    except getopt.GetoptError:
+#        usage()    
+#
+#
+#    server = user = password = vhost = timeout = in_queue = out_queue = message = callback = None
+#    for opt, arg in opts:
+#        if opt == '-h':
+#            usage()
+#        elif opt in ("-S", "--sender"):
+#            message  = rabbitcoat.default_message
+#        elif opt in ("-R", "--receiver"):
+#            callback  = rabbitcoat.default_callback
+#        elif opt in ("-s", "--server"):
+#            server = arg
+#        elif opt in ("-u", "--user"):
+#            user = arg
+#        elif opt in ("-p", "--password"):
+#            password = arg
+#        elif opt in ("-v", "--vhost"):
+#            vhost = arg
+#        elif opt in ("-q", "--in_queue"):
+#            in_queue = arg
+#        elif opt in ("-Q", "--out_queue"):
+#            out_queue = arg
+#        elif opt in ("-m", "--message"):
+#            message = arg
+#        elif opt in ("-t", "--timeout"):
+#            timeout = arg
+#        else:
+#            usage()
+#
+#    
+#    rabbit = rabbitcoat(server,user,password,vhost,timeout,in_queue,out_queue,message,callback)
+#    rabbit.run();
+#        
+#        
 
-
-def usage():
-    print 'wrapper.py [--sender/--receiver] [-s <server>] [-u <user>] [-p <password>] [-v <vhost>] [-q <in_queue>] [-Q <out_queue>] [-m <message>] [-t <timeout>]'
-    sys.exit()
-
-
-
-	
 def main(argv):
-    try:
-        opts, args = getopt.getopt(argv,"SRhs:u:p:v:q:Q:m:t:",
-                                   ["receiver""sender","server=",
-                                    "user=","password=","vohst=",
-                                    "in_queue=","out_queue=",
-                                    "message=","timeout="])
-    except getopt.GetoptError:
-        usage()    
+    rabbit = rabbitcoat()
+    rabbit.Receive()
+    #print "testers unimplemented *yet*- use cli.py"
 
-
-    server = user = password = vhost = timeout = in_queue = out_queue = message = callback = None
-    for opt, arg in opts:
-        if opt == '-h':
-            usage()
-        elif opt in ("-S", "--sender"):
-            message  = rabbitcoat.default_message
-        elif opt in ("-R", "--receiver"):
-            callback  = rabbitcoat.default_callback
-        elif opt in ("-s", "--server"):
-            server = arg
-        elif opt in ("-u", "--user"):
-            user = arg
-        elif opt in ("-p", "--password"):
-            password = arg
-        elif opt in ("-v", "--vhost"):
-            vhost = arg
-        elif opt in ("-q", "--in_queue"):
-            in_queue = arg
-        elif opt in ("-Q", "--out_queue"):
-            out_queue = arg
-        elif opt in ("-m", "--message"):
-            message = arg
-        elif opt in ("-t", "--timeout"):
-            timeout = arg
-        else:
-            usage()
-
-    
-    rabbit = rabbitcoat(server,user,password,vhost,timeout,in_queue,out_queue,message,callback)
-    rabbit.run();
-        
-        
 
 if __name__ == "__main__":
     main(sys.argv[1:])
