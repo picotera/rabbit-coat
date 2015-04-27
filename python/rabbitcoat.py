f9efc82673fb1ad7f0a44c9345b81197b67baa00
kobybum <kobybum@gmail.com>
2015-04-27 16:06:32 +0300

    Some bug fixes
    There were a lot of little bugs and code that was not needed
    
    Note the design I made for the responder, and see if you like it
    I didn't check if the responder works yet though, maybe some syntax
    errors or something.
    
    I did basic tests for receiver and sender in the google app
    
    I commented started with CR: for comments on why I changed stuff

--- python/rabbitcoat.py
+++ python/rabbitcoat.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+import threading
 import pika, os, uuid, logging,sys, ConfigParser
+import json
+
 logging.basicConfig()
 
+#####################################################################################################################
+# CR: I'll write all my comments starting with CR: if you don't mind, so you'll understand why I made those changes #
+# TODO: I'll write stuff starting with TODO if it need to be done eventually                                        #
+#####################################################################################################################
 
+# For cli
+default_configuration_file = 'rabbitcoat.conf'
 
-def validate(value,current,default=None):
+def validate(value, current, default=None):
     if ((value is None) or (value == "")):
         if ((not (default is None)) and ((current is None) or (current == ""))):
             return default
@@ -13,44 +22,36 @@ def validate(value,current,default=None):
     else:
         return value
 
-#class rabbitcoat(Rabbit): 
-
-
 class RabbitFrame(object):
-    default_configuration_file = "rabbitcoat.conf"
-   def __init__(self,conf=None,server=None, user=None, password=None, vhost=None, timeout=None):
+
+    def __init__(self, config, server=None, user=None, password=None, vhost=None, timeout=None):
         self.server = self.user = self.password = self.vhost = self.timeout = None
-        self.__loadConfiguration(conf)
+        
+        self.config = config
+        self.__loadConfiguration()
         self.__initInfrastructure(server, user, password, vhost, timeout)
 
+    def __loadConfiguration(self):
 
-    def __loadConfiguration(self,conf=None):
-        if (conf is None):
-            configFile = self.default_configuration_file
-        else:
-            configFile = conf
-
-        config = ConfigParser.SafeConfigParser(allow_no_value=True)
-        config.read(configFile)
+        parser = ConfigParser.SafeConfigParser(allow_no_value=True)
+        parser.read(self.config)
         
-        self.server    = validate(config.get('infrastructure', 'server')      ,self.server)
-        self.user      = validate(config.get('infrastructure', 'user')        ,self.user)
-        self.vhost     = validate(config.get('infrastructure', 'vhost')       ,self.vhost    ,self.user)
-        self.password  = validate(config.get('infrastructure', 'password')    ,self.password)
-        self.timeout   = validate(config.get('infrastructure', 'timeout')     ,self.timeout  ,5)
+        #CR: RABBITMQ allows it to be inside a bigger config file, if needed ( more specific name )
+        #CR: This validation also has no meaning because everything is None
+        self.server    = validate(parser.get('RABBITMQ', 'server')      ,self.server)
+        self.user      = validate(parser.get('RABBITMQ', 'user')        ,self.user)
+        self.vhost     = validate(parser.get('RABBITMQ', 'vhost')       ,self.vhost    ,self.user)
+        self.password  = validate(parser.get('RABBITMQ', 'password')    ,self.password)
+        self.timeout   = validate(parser.getint('RABBITMQ', 'timeout')     ,self.timeout  ,5)
 
-
-
-    def __setInfrastructure(self,server=None, user=None, password=None,vhost=None,timeout=None):
+    def __setInfrastructure(self, server=None, user=None, password=None, vhost=None, timeout=None):
         self.server   = validate(server,self.server)
         self.user     = validate(user,self.user)
         self.password = validate(password,self.password)
         self.vhost    = validate(vhost,self.vhost)
         self.timeout  = validate(timeout,self.timeout)
 
-
-
-    def __initInfrastructure(self,server=None, user=None, password=None,vhost=None,timeout=None):
+    def __initInfrastructure(self, server=None, user=None, password=None, vhost=None, timeout=None):
         self.__setInfrastructure(server, user, password, vhost, timeout) #override config file if the user wants
         ampq_url = "amqp://%s:%s@%s/%s" % (self.user,self.password,self.server,self.vhost)
         url = os.environ.get('CLOUDAMQP_URL',ampq_url)
@@ -60,101 +61,140 @@ class RabbitFrame(object):
         self.connection = pika.BlockingConnection(params) # Connect to CloudAMQP
         self.channel = self.connection.channel() # start a channel
 
-
-
-
+        
 class RabbitSender(RabbitFrame):
     
     def __init__(self, config, queue, reply_to=None):
-        RabbitChannel.__init__(self, config)
+        RabbitFrame.__init__(self, config)
         
         self.queue = queue
         self.channel.queue_declare(queue, durable=True)
         self.reply_to = reply_to       
 
-    def Send(self,data=None,corr_id=str(uuid.uuid4()),reply_to_queue=None,channel=None):
+    def Send(self, data=None, corr_id=str(uuid.uuid4()), reply_to_queue=None):
         message        = json.dumps(data)
-        reply_to_queue = validate(reply_to_queue,self.reply_to)
+        reply_to_queue = validate(reply_to_queue, self.reply_to)
 
-        channel = validate(channel,self.channel)
+        #CR: You can't send on a used channel, that's why we have a channel for each sender/receiver.
+        #CR: The queue is also declared in the constructor for the same reason
 
-        channel.queue_declare(queue,durable=True) # Declare a queue
         # send a message
-        channel.basic_publish(exchange='', 
-                              routing_key=self.queue, 
-                              body=message,
-                              properties=pika.BasicProperties(
-                                  delivery_mode = 2, # make message persistent
-                                  correlation_id = corr_id,
-                                  reply_to = reply_to_queue,
-                              ))
+        self.channel.basic_publish(exchange='', 
+                                   routing_key=self.queue, 
+                                   body=message,
+                                   properties=pika.BasicProperties(
+                                       delivery_mode = 2, # make message persistent
+                                       correlation_id = corr_id,
+                                       reply_to = reply_to_queue,
+                                   ))
+                              
+        #TODO: This should be moved to a log eventually
         print "Sender: Produced message with:"
         print "\t correlation ID: %s" % corr_id
         print "\t body: %s" % message
 
-
-
+        
 class RabbitReceiver(RabbitFrame, threading.Thread):
 
-     def __init__(self, config, queue, callback,read_repeatedly=False):
-        RabbitChannel.__init__(self, config)
+    def __init__(self, config, queue, callback, read_repeatedly=False):
+        RabbitFrame.__init__(self, config)
         threading.Thread.__init__(self, name='RabbitReceiver %s' %queue)
         
-        self.queue = validate(queue,self.in_queue)
-        self.channel.queue_declare(queue,durable=True) # Declare a queue
+        self.queue = queue
+        self.channel.queue_declare(queue, durable=True) # Declare a queue
 
-        self.read_repeatedly = read_repeatedly        
-        self.callback = validate(callback,self.callback)
-
-
+        self.read_repeatedly = read_repeatedly
+        #CR: self.callback not defined
+        self.callback = callback
 
     def run(self):
         channel = self.channel
         ''' Bind a callback to the queue '''
+        #TODO: This should be moved to a log eventually
         print "Receiver: starting to consume messeges"
-        
+
         # set up subscription on the queue
         channel.basic_qos(prefetch_count=1)
-        
+
         channel.basic_consume(self.callback,
                               self.queue,
                               no_ack=self.read_repeatedly)
-        
+
         channel.start_consuming()
 
 
-
+# A basic print response for debugging
+def printResponse(body):
+    return "response: got %s" % str(body)
+    
 class RabbitResponder(RabbitReceiver):
 
-     def __init__(self, config, inbound_queue, outbound_queue, response_function,read_repeatedly=False):
-         RabbitReceiver.__init__(self, config, queue, Respond,read_repeatedly)
-         self.response_function = validate(response_function,self.response_function,self.default_response)
+    def __init__(self, config, inbound_queue, response_function, default_out_queue=None, read_repeatedly=False):
+        RabbitReceiver.__init__(self, config, queue, self.__callback, read_repeatedly)
+        
+        '''
+        CR: This is a design I thought of... How about we make a constant sender for each queue we're using?
+        Most of the gears will be using one outbound queue, and it would be inefficient to declare a sender each time
+        Some gears (which work with many others, like the parser that parses many articles) work with many queues,
+        So why not create a sender for each queue we encounter? Since we'll probably use it a lot, and there aren't too many queues
+        
+        Feel free to change this but I think it's better than the other option
+        '''
+        self.senders = {}
+        # In case outbound queue isn't specified, reply_to MUST be specified in every message!
+        if default_out_queue != None:
+            sender = RabbitSender(self.config, default_out_queue)
+            # Define a default sender, when reply_to is None
+            self.senders[None] = sender
+            self.senders[default_out_queue] = sender
+            
+        #CR: No need to validate.. why would you use a default response other than debugging?
+        self.response_function = self.response_function
 
-
-    def Respond(self, ch, method, properties, body):
+    def __callback(self, ch, method, properties, body):
+        '''
+        Respond to the relevant queue
+        '''        
         response = self.response_function(body)
-        sender = RabbitSender(self.config, properties.reply_to, inbound_queue)        
-        sender.Send(self,data=response,corr_id=properties.correlation_id,channel=ch):
+        reply_to = properies.reply_to
+        
+        if self.senders.has_key(reply_to):
+            sender = self.senders[reply_to]
+        # We have no idea where to send the response
+        elif reply_to is None:
+            #TODO: Logging
+            print 'ERROR: no where to reply'
+            ch.basic_ack(delivery_tag = method.delivery_tag)
+            return
+        else:
+            sender = RabbitSender(self.config, reply_to)
+            self.senders[reply_to] = sender
+        
+        ''' CR: The other option
+        # Only declare a new sender if it's different from the constant one
+        if (reply_to != None) and (reply_to != self.outbound_queue):
+            sender = RabbitSender(self.config, properties.reply_to, inbound_queue)
+        '''
+        
+        sender.Send(self, data=response, corr_id=properties.correlation_id)        
+        
         ch.basic_ack(delivery_tag = method.delivery_tag)
-
-
-    def default_response(self,body):
-        return "response: got %s" % str(body)
-
-
-
+    
+    
 class RabbitRequester(RabbitSender):
 
     def __init__(self, config, outbound_queue, callback, inbound_queue, read_repeatedly=False):
         RabbitSender.__init__(self, config, outbound_queue, inbound_queue)
 
-
     def ask(self,send_queue=None, reply_to_queue=None,message=None,callback=None,corr_id=str(uuid.uuid4())):
         self.Send(send_queue, message,corr_id,reply_to_queue)
+
+        '''
+        CR: Not changing this because I don't need it, but you send one message and the thread goes on forever, not a good idea
+        '''
         receiver = RabbitReceiver(config, reply_to_queue, callback,False)
         receiver.run()
-
-
+        
 
 def main(argv):
     #rabbit = rabbitcoat()
